375e2972050bc31a8d0f04fb9f7980216386fec2
Aakash <codewithaakash7@gmail.com>
2024-05-10 13:06:52 -0700

    added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,17 +10,11 @@ import Home from '../components/Home'
 
 
 const getURL = () => {
-    let url =
-        import.meta.env.NEXT_PUBLIC_VERCEL_URL;
-    // import.meta?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    // import.meta?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    // 'http://localhost:5173/'
-    // Make sure to include `https://` when not localhost.
-    // url = url.includes('http') ? url : `https://${url}`
-    // Make sure to include a trailing `/`.
-    // url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-    return url
-}
+    let url = import.meta?.env?.NEXT_PUBLIC_SITE_URL ?? import.meta?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:5173/';
+    url = url.includes('http') ? url : `https://${url}`;
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    return url;
+};
 
 
 const supabase = createClient(
