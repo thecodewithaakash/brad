93a8885b3c42e04e6d1f1bae0333deaa14eb7142
Aakash <codewithaakash7@gmail.com>
2024-05-09 18:23:12 -0700

    updated Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = ({ logOut, session }: any) => {
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
-                    <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Brad K</h2>
+                    <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">{session !== null ? session.user.user_metadata.full_name : "Brad K"}</h2>
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-indigo-500">Welcome to User Management Platform</h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Welcome to our User Management App,where people login,signup & Update their account Information</p>
                 </div>
